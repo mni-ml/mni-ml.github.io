11e58e24294e1604572995e836e004211e85b6f0
Aadi Kulshrestha <aadi.kulsh@shopify.com>
2026-04-16 17:18:06 -0400

    dynamic importing of blog posts

--- src/lib/articles.ts
+++ src/lib/articles.ts
@@ -9,3 +9,12 @@ export async function getAllArticles() {
     .filter((a) => !a.data.draft)
     .sort((a, b) => a.data.order - b.data.order);
 }
+
+export async function getPrevNextArticle(slug: string) {
+  const articles = await getAllArticles();
+  const idx = articles.findIndex((a) => a.id === slug);
+  return {
+    prev: idx > 0 ? articles[idx - 1] : undefined,
+    next: idx < articles.length - 1 ? articles[idx + 1] : undefined,
+  };
+}
